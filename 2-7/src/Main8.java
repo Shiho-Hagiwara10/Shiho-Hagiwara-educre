8c8ff2292d794f774a800579cda9b144cd28200d
Shiho-Hagiwara10 <qiuyuanshisui@gmail.com>
2025-10-23 23:42:20 +0900

    Add files via upload

--- 2-7/src/Main8.java
+++ 2-7/src/Main8.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
 public class Main8 {
 public static void main(String[] args) {
-    int attack = 50;
-    int defence = 20;
-    int damege = attack-defence;
-    double critical = damege*1.5;
-    System.out.println("攻撃力:" + attack);
-    System.out.println("防御力:" + defence);
-    System.out.println("通常ダメージ:" + damege);
-    System.out.println("クリティカルダメージ:" + (int)critical);
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("攻撃力を入力してください:");
+    int attack = sc.nextInt();
+
+    System.out.println("防御力を入力してください:");
+    int defence = sc.nextInt();
+
+    int normalDamege = attack-defence;
+    int criticalDamege = (int)(normalDamege*1.5);
+
+    System.err.println("通常ダメージ:" + normalDamege);
+    System.out.println("クリティカルダメージ:" + criticalDamege);
+
+    sc.close();
+
 }
 }
