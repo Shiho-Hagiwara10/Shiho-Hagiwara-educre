260d3d84fde5f65d850dc7f1b77c51f50cd354de
Shiho-Hagiwara10 <qiuyuanshisui@gmail.com>
2025-10-25 22:31:22 +0900

    Add files via upload

--- 2-7/src/Main3.java
+++ 2-7/src/Main3.java
@@ -9,7 +9,7 @@ public class Main3 {
         int defense=5;
         //ダメージを計算
         //整数同士の計算では小数点以外は切り捨てられる
-        int damage=(attack*2-defense)/3;
+        int damage = (attack * 2 - defense)/3;
         //結果を表示
         System.out.println("与えたダメージ:" + damage);
 
--- 2-7/src/Main8.java
+++ 2-7/src/Main8.java
@@ -3,17 +3,17 @@ public class Main8 {
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("攻撃力を入力してください:");
+    System.out.println("攻撃力を入力してください:");
     int attack = Integer.parseInt(scanner.nextLine());
 
-    System.out.print("防御力を入力してください:");
+    System.out.println("防御力を入力してください:");
     int defense = Integer.parseInt(scanner.nextLine());
 
     int normalDamage = attack-defense;
     int criticalDamage = (int)(normalDamage*1.5);
 
-    System.out.print("通常ダメージ:" + normalDamage);
-    System.out.print("クリティカルダメージ:" + criticalDamage);
+    System.out.println("通常ダメージ:" + normalDamage);
+    System.out.println("クリティカルダメージ:" + criticalDamage);
 
     scanner.close();
 
