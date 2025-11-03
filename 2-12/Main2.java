e514084d4e101ea4a93dd3f10bb632f5762743ab
Shiho-Hagiwara10 <qiuyuanshisui@gmail.com>
2025-11-03 21:39:54 +0900

    Add files via upload

---
+++ 2-12/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 5; x ++) {
+            for (int y = 1; y <= 9; y ++) {
+                System.out.println(x + "×" + y + "=" + (x * y));
+            }
+            System.out.println();
+        }
+    }
+}
